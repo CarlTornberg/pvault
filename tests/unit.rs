7635c0c4ec0fe384417ca766c5d7cfbddf3e4078
Carl Tornberg <carltornberg@hotmail.com>
2025-11-25 19:47:34 +0100

    Removed unused unit tests for now until a structure is determined

--- tests/unit.rs
+++ tests/unit.rs
@@ -2,32 +2,4 @@
 #[allow(unused_imports)]
 #[cfg(test)]
 mod unit {
-    use std::collections::HashSet;
-    use std::hash::Hash;
-
-    use pvault::*;
-
-    #[test]
-    fn handler_discriminator_check() {
-        let discriminators = [
-            pvault::InitializeProc::DISCRIMINATOR,
-        ];
-
-        assert!(is_unique(discriminators));
-    }
-    #[test]
-    fn ha_discriminator_check() {
-        let discriminators = [
-            pvault::Vault::DISCRIMINATOR,
-        ];
-
-        assert!(is_unique(discriminators));
-    }
-
-    fn is_unique<T>(iter: T) -> bool where
-        T: IntoIterator,
-        T::Item: Eq + Hash, {
-        let mut hash_set = HashSet::new();
-        iter.into_iter().all(|d| hash_set.insert(d))
-    }
 }
