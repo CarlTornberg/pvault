d870ce11a685e1d6d63d90f7cceffad2e1268aaf
Carl Tornberg <carltornberg@hotmail.com>
2025-11-25 19:45:32 +0100

    Refactoring

--- src/processors/initialize.rs
+++ src/processors/initialize.rs
@@ -1,9 +1,12 @@
-use pinocchio::{account_info::AccountInfo, program_error::ProgramError, sysvars::{rent, Sysvar}, ProgramResult};
+use pinocchio::{ProgramResult, account_info::AccountInfo, instruction::{AccountMeta, Instruction}, program_error::ProgramError, pubkey::Pubkey, sysvars::{Sysvar, rent::Rent}};
 use pinocchio_system::instructions::CreateAccount;
 
-use crate::{Instruction, Vault};
+pub const DISCRIMINATOR: u8 = 0;
 
-pub type InitializeProc<'a> = Instruction<'a, InitializeAccounts<'a>, InitializeData<'a>>;
+pub struct Initialize<'a> {
+    pub accounts: InitializeAccounts<'a>,
+    pub instruction_data: InitializeInstructionData<'a>,
+}
 
 pub struct InitializeAccounts<'a> {
     /// 0: [w]: Initialized, Allocated, Assigned, Signer, writable, payer,
@@ -16,77 +19,75 @@ pub struct InitializeAccounts<'a> {
     pub vault: &'a AccountInfo,
 }
 
+impl<'a> InitializeAccounts<'a> {
+    pub fn as_array(&'a self) -> [&'a Pubkey; 3] {
+        [self.authority.key(), self.vault_data.key(), self.vault.key()]
+    }
+}
+
 #[repr(C)]
-pub struct InitializeData<'a> {
+pub struct InitializeInstructionData<'a> {
     pub locked: &'a bool,
 }
 
-impl<'a> Instruction<'a, InitializeAccounts<'a>, InitializeData<'a>> {
-    pub const DISCRIMINATOR:&'a u8 = &0;
+impl<'a> InitializeInstructionData<'a> {
+    pub fn pack(&'a self) -> [u8; 2] {
+        [DISCRIMINATOR, *self.locked as u8]
+    }
+}
 
-    pub fn process(&self) -> ProgramResult {
-            
-        let space = core::mem::size_of::<Vault>();
+impl<'a> Initialize<'a> {
+    pub fn process(&'a self) -> ProgramResult {
+
+        let space = core::mem::size_of::<InitializeInstructionData>();
+        let rent = Rent::get()?.minimum_balance(space);
+
         CreateAccount {
             from: self.accounts.authority,
             to: self.accounts.vault_data,
+            lamports: rent,
             space: space as u64,
-            lamports: rent::Rent::get()?.minimum_balance(space),
             owner: &pinocchio_system::ID,
-        }
-        .invoke()?;
-
+        }.invoke()?;
+        
         Ok(())
     }
 }
 
-impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Instruction<'a, InitializeAccounts<'a>, InitializeData<'a>>  {
+impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Initialize<'a> {
     type Error = ProgramError;
-
     fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
-        let (data, accounts) = value;
-
-        // Check accounts
-        if accounts.len() < 3 {
+        let (data, accounts_slice) = value;
+        if accounts_slice.len() < 3 {
             return Err(ProgramError::NotEnoughAccountKeys);
         }
-    
-        // Authority 
-        let authority = &accounts[0];
-        if !authority.is_signer() {
+        let accounts = InitializeAccounts{
+            authority: &accounts_slice[0],
+            vault_data: &accounts_slice[1],
+            vault: &accounts_slice[2],
+        };
+
+        if !accounts.authority.is_signer() {
             return Err(ProgramError::MissingRequiredSignature);
         }
-        
-        // Vault data 
-        let vault_data = &accounts[1];
-        if !vault_data.is_writable() {
-            return Err(ProgramError::Custom(0));
-        }
-        if vault_data.lamports() != 0 {
+
+        if accounts.vault_data.lamports() != 0 {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
 
-        // Vault 
-        let vault = &accounts[2];
-        if !vault.is_writable() {
-            return Err(ProgramError::Custom(0));
-        }
-        if vault.lamports() != 0 {
+        if accounts.vault.lamports() != 0 {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
 
-        // Check data
+        // Data
         if data.len() != core::mem::size_of::<bool>() {
             return Err(ProgramError::InvalidInstructionData);
         }
-        let locked = unsafe { &*(data.as_ptr() as *const bool) };
 
-        Ok(Self {
-            discriminator: Self::DISCRIMINATOR,
-            accounts: InitializeAccounts {
-                authority, vault_data, vault,
-            },
-            data: InitializeData{ locked },
-        })
+        let locked = unsafe { &*(data.as_ptr() as *const bool) }; 
+        let instruction_data = InitializeInstructionData{ locked };
+
+        Ok( Self{ accounts, instruction_data}) 
     }
 }
+
