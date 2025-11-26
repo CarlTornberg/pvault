07568419e01a9493be1a89ead3fd20b9701712a4
Carl Tornberg <carltornberg@hotmail.com>
2025-11-26 10:37:49 +0100

    Added bumps and fixed priv issues when creating accounts

--- src/processors/initialize.rs
+++ src/processors/initialize.rs
@@ -1,4 +1,4 @@
-use pinocchio::{ProgramResult, account_info::AccountInfo, msg, program_error::ProgramError, pubkey, sysvars::{Sysvar, rent::Rent}};
+use pinocchio::{ProgramResult, account_info::AccountInfo, instruction::Signer, msg, program_error::ProgramError, pubkey, seeds, sysvars::{Sysvar, rent::Rent}};
 use pinocchio_system::instructions::CreateAccount;
 
 use crate::Vault;
@@ -16,9 +16,11 @@ pub struct InitializeAccounts<'a> {
 
     /// 1: [w]: Not initialized
     pub vault_data: &'a AccountInfo,
+    pub vault_data_bump: u8,
 
     /// 2: [w]: Not initialized
     pub vault: &'a AccountInfo,
+    pub vault_bump: u8,
 }
 
 #[repr(C)]
@@ -35,42 +37,8 @@ impl<'a> InitializeInstructionData<'a> {
         )
     }
     
-    pub fn get_packed_instruction_data(locked: bool) -> [u8; 1] {
-        [locked as u8]
-    }
-}
-
-impl<'a> Initialize<'a> {
-    pub fn process(&'a self) -> ProgramResult {
-        let space = core::mem::size_of::<InitializeInstructionData>();
-        let rent = Rent::get()?.minimum_balance(space);
-
-        CreateAccount {
-            from: self.accounts.authority,
-            to: self.accounts.vault_data,
-            lamports: rent,
-            space: space as u64,
-            owner: &crate::ID,
-        }.invoke()?;
-        msg!("Vault data created");
-
-        CreateAccount {
-            from: self.accounts.authority,
-            to: self.accounts.vault,
-            lamports: Rent::get()?.minimum_balance(0),
-            space: 0,
-            owner: &pinocchio_system::ID,
-        }.invoke()?;
-        msg!("Vault created");
-
-        if let Ok(mut data) = self.accounts.vault_data.try_borrow_mut_data() {
-            let new_data = unsafe { self.instruction_data.pack() };
-            data.copy_from_slice(new_data);
-            msg!("Vault data set");
-        }
-        else { return Err(ProgramError::AccountBorrowFailed); }
-
-        Ok(())
+    pub fn get_packed_instruction_data(locked: bool) -> [u8; 2] {
+        [DISCRIMINATOR, locked as u8]
     }
 }
 
@@ -81,32 +49,37 @@ impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Initialize<'a> {
         if accounts_slice.len() < 3 {
             return Err(ProgramError::NotEnoughAccountKeys);
         }
-        let accounts = InitializeAccounts{
-            authority: &accounts_slice[0],
-            vault_data: &accounts_slice[1],
-            vault: &accounts_slice[2],
-        };
+        let authority = &accounts_slice[0];
+        let vault_data = &accounts_slice[1];
+        let vault = &accounts_slice[2];
 
-        if !accounts.authority.is_signer() {
+        if !authority.is_signer() {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
-        let (vault_data, _vault_data_bump) = Vault::get_vault_data_pda(accounts.authority.key());
-        if !pubkey::pubkey_eq(accounts.vault_data.key(), &vault_data) {
+        let (vault_data_pda, vault_data_bump) = Vault::get_vault_data_pda(authority.key());
+        if !pubkey::pubkey_eq(vault_data.key(), &vault_data_pda) {
             return Err(ProgramError::InvalidSeeds);
         }
 
-        if accounts.vault_data.lamports() != 0 {
+        if vault_data.lamports() != 0 {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
 
-        let (vault, _vault_bump) = Vault::get_vault_pda(accounts.authority.key());
-        if !pubkey::pubkey_eq(accounts.vault.key(), &vault) {
+        let (vault_pda, vault_bump) = Vault::get_vault_pda(authority.key());
+        if !pubkey::pubkey_eq(vault.key(), &vault_pda) {
             return Err(ProgramError::InvalidSeeds);
         }
-        if accounts.vault.lamports() != 0 {
+        if vault.lamports() != 0 {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
+        let accounts = InitializeAccounts { 
+            authority, 
+            vault_data, 
+            vault_data_bump, 
+            vault, 
+            vault_bump 
+        };
 
         // Data
         if data.len() != core::mem::size_of::<bool>() {
@@ -120,3 +93,48 @@ impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Initialize<'a> {
     }
 }
 
+impl<'a> Initialize<'a> {
+    pub fn process(&'a self) -> ProgramResult {
+        let space = core::mem::size_of::<InitializeInstructionData>();
+        let rent = Rent::get()?.minimum_balance(space);
+
+        let data_bump = &[self.accounts.vault_data_bump];
+        let vault_data_seeds = seeds!(
+            Vault::VAULT_DATA_SEED, 
+            self.accounts.authority.key(), 
+            data_bump
+        );
+        CreateAccount {
+            from: self.accounts.authority,
+            to: self.accounts.vault_data,
+            lamports: rent,
+            space: space as u64,
+            owner: &crate::ID,
+        }.invoke_signed(&[Signer::from(&vault_data_seeds)])?;
+        msg!("Vault data created");
+
+        let vault_bump = &[self.accounts.vault_bump];
+        let vault_seeds = seeds!(
+            Vault::VAULT_SEED, 
+            self.accounts.authority.key(),
+            vault_bump
+        );
+        CreateAccount {
+            from: self.accounts.authority,
+            to: self.accounts.vault,
+            lamports: Rent::get()?.minimum_balance(0),
+            space: 0,
+            owner: &pinocchio_system::ID,
+        }.invoke_signed(&[Signer::from(&vault_seeds)])?;
+        msg!("Vault created");
+
+        if let Ok(mut data) = self.accounts.vault_data.try_borrow_mut_data() {
+            let new_data = unsafe { self.instruction_data.pack() };
+            data.copy_from_slice(new_data);
+            msg!("Vault data set");
+        }
+        else { return Err(ProgramError::AccountBorrowFailed); }
+
+        Ok(())
+    }
+}
